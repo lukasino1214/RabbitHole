f6aecbc9e0102d04b1c7f6c1a85992522fb74507
lukasino1214 <38435841+lukasino1214@users.noreply.github.com>
2021-02-23 18:14:26 +0100

    Camera movement

--- RabbitHole/src/Application.cpp
+++ RabbitHole/src/Application.cpp
@@ -12,7 +12,6 @@
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Texture.h"
-#include "Shader.h"
 
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
@@ -116,7 +115,55 @@ int main(void) {
         /*float r = 0.0f;
         float increment = 0.05f;*/
 
+        float mouseSpeed = 0.005f;
+        float horizontalAngle = 3.14f;
+        float verticalAngle = 0.0f;
+        float speed = 0.005f;
+        glm::vec3 position = glm::vec3(3, -4, -3);
+
         while (!glfwWindowShouldClose(window)) {
+            static double lastTime = glfwGetTime();
+            double currentTime = glfwGetTime();
+            float deltaTime = float(currentTime - lastTime);
+
+            double xpos, ypos;
+            glfwGetCursorPos(window, &xpos, &ypos);
+
+            glfwSetCursorPos(window, 1024 / 2, 576 / 2);
+
+            horizontalAngle -= mouseSpeed * float(1024 / 2 - xpos);
+            verticalAngle += mouseSpeed * float(576 / 2 - ypos);
+
+            glm::vec3 direction(cos(verticalAngle) * sin(horizontalAngle), sin(verticalAngle), cos(verticalAngle) * cos(horizontalAngle));
+
+            glm::vec3 right = glm::vec3(sin(horizontalAngle - 3.14f / 2.0f), 0, cos(horizontalAngle - 3.14f / 2.0f));
+
+            // Up vector
+            glm::vec3 up = glm::cross(right, direction);
+
+            // Move forward
+            if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+                position += direction * deltaTime * speed;
+            }
+            // Move backward
+            if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+                position -= direction * deltaTime * speed;
+            }
+            // Strafe right
+            if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+                position += right * deltaTime * speed;
+            }
+            // Strafe left
+            if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+                position -= right * deltaTime * speed;
+            }
+
+            View = glm::lookAt(
+                position,           // Camera is here
+                position + direction, // and looks here : at the same position, plus "direction"
+                up                  // Head is up (set to 0,-1,0 to look upside-down)
+            );
+
             renderer.Clear();
 
             ImGui_ImplOpenGL3_NewFrame();
